b1e524bd5b73d740759898515e22e197a5af9b8c
jitesh0311 <jiteshsalian1713@gmail.com>
2023-09-08 19:59:42 +0530

    completed-projected

--- Backend/server.js
+++ Backend/server.js
@@ -1,58 +1,24 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-
-
+const winston = require("winston");
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
-
-app.use(
-  cors({
-    origin: "http://localhost:3000", // Your frontend URL
-  })
-);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(morgan("combined"));
+winston.add(new winston.transports.File({ filename: "error.log" }));
 
 
-var currentDate = new Date();
 
-var year = currentDate.getFullYear();
-var month = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
-var day = currentDate.getDate() ; // Remove this after testing
-
-var formatemonth = month < 10 ? "0" + month : month;
-var formateday = day < 10 ? "0" + day : day;
-
-var formattedDate = `${formateday}/${formatemonth}/${year}`;
-
-function getCurrentTime() {
-  var hours = currentDate.getHours();
-  var minutes = currentDate.getMinutes();
-
-  var hours = currentDate.getHours();
-  var minutes = currentDate.getMinutes();
-  var ampm = hours >= 12 ? "PM" : "AM";
-  var formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-
-  var formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
-
-  var currentTime = `${formattedHours}-${formattedMinutes}-${ampm}`;
-
-  return currentTime;
-}
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "csv_db 6",
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.get("/", (_req, res) => {
@@ -60,88 +26,80 @@ app.get("/", (_req, res) => {
 });
 
 app.get("/futuredata", async (req, res) => {
-  const limitValues = Array.from({ length: 49 }, (_, index) => index + 1); // Add more limit values as needed
+  const limitValues = Array.from({ length: 49 }, (_, index) => index + 1);
 
-  const results = [];
+  try {
+    const results = await Promise.all(
+      limitValues.map(async (limit) => {
+        const sql = `SELECT * FROM futuredata WHERE Date = '${formattedDate}' LIMIT ${limit}`;
+        const data = await executeQuery(sql);
+        return data;
+      })
+    );
 
-  // Use async/await to ensure sequential execution of queries
-  for (const limit of limitValues) {
-    const sql = `SELECT * FROM futuredata WHERE Date = '${formattedDate}' LIMIT ${limit}`;
-
-    try {
-      const data = await new Promise((resolve, reject) => {
-        db.query(sql, (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        });
-      });
-
-      results.push(data);
-    } catch (error) {
-      console.error(error);
-    }
+    return res.json(results);
+  } catch (error) {
+   console.error("Error updating data:", error);
+   winston.error("Error updating data:", error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while fetching data" });
   }
-
-  return res.json(results);
 });
 
-app.post("/update", (req, res) => {
+app.post("/update", async (req, res) => {
   const formData = req.body;
-  
 
   const updateQuery =
     "UPDATE futuredata SET BhutanGold=?, BhutanDeluxe=? WHERE Date=? AND Time=?";
 
   const { Date, Time, BhutanGold, BhutanDeluxe } = formData;
 
-  db.query(
-    updateQuery,
-    [BhutanGold, BhutanDeluxe, Date, Time],
-    (error, results) => {
-      if (error) {
-        console.error("Error updating data:", error);
-        res
-          .status(500)
-          .json({ error: "An error occurred while updating data" });
-      } else {
-        res.json({ message: "Form data updated successfully" });
-      }
-    }
-  );
+  try {
+    await executeQuery(updateQuery, [BhutanGold, BhutanDeluxe, Date, Time]);
+    return res.json({ message: "Form data updated successfully" });
+  } catch (error) {
+    console.error("Error updating data:", error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while updating data" });
+  }
 });
+
+app.get("/showAdmin", async (_req, res) => {
+  const sql = `SELECT * FROM futuredata WHERE Date = '${formattedDate}'`;
+
+  try {
+    const data = await executeQuery(sql);
+    return res.json(data);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while fetching admin data" });
+  }
+});
+
+const executeQuery = (sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.query(sql, params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+const formatemonth = month < 10 ? "0" + month : month;
+const formateday = day < 10 ? "0" + day : day;
+const formattedDate = `${formateday}/${formatemonth}/${year}`;
 
 app.listen(8081, () => {
   console.log("Server is listening on port 8081");
-});
-
-app.get("/showAdmin", async (req, res) => {
-
-
-  const results = [];
-
-  // Use async/await to ensure sequential execution of queries
-   {
-    const sql = `SELECT * FROM futuredata WHERE Date = '${formattedDate}'`;
-
-    try {
-      const data = await new Promise((resolve, reject) => {
-        db.query(sql, (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        });
-      });
-
-      results.push(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  return res.json(results);
 });
